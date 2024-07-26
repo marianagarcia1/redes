5b88bbaff67db9e24477745c06f7378d41e63197
Mauricio Godoy <mauriciomgodoy01@gmail.com>
2024-07-26 15:03:34 -0300

    atuador atualizado

--- atuador/atuador.ino
+++ atuador/atuador.ino
@@ -1,71 +1,67 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
-#include <Timer.h>
+#include <Ticker.h>
 #define LED_PIN 2
 
 WiFiUDP udp;
-char packetBuffer[255];
 unsigned int localPort = 1322;
 const char *ssid = "mauricio";
 const char *password = "olnu7936";
 
 int pwmRecebido = 0;
-int inicioTempo = 0;
-int finalTempo = 0;
-Timer send_message;
+unsigned long inicioTempo = 0;
+unsigned long finalTempo = 0;
+Ticker sendTemperatureTicker;
+float temperatura = 25.0; // Valor inicial da temperatura
 
 void ValidatePWM(int pwm) {
-  if(pwm < 255 && pwm > 0){
+  if (pwm < 255 && pwm > 0) {
     pwmRecebido = pwm;
     inicioTempo = millis();
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  // Connect to Wifi network.
-  WiFi.begin(ssid, password);
-  
-  pinMode(LED_PIN, OUTPUT);
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500); Serial.print(F("."));
-  }
-  udp.begin(localPort);
-  Serial.printf("UDP server : %s:%i \n", WiFi.localIP().toString().c_str(), localPort);
-  digitalWrite(LED_PIN, HIGH);
-
-
+void SendTemperature() {
+  udp.beginPacket(udp.remoteIP(), udp.remotePort());
+  udp.write((uint8_t*)&temperatura, sizeof(temperatura));  // Envia o valor da temperatura diretamente como bytes
+  udp.endPacket();
+  Serial.printf("Temperatura enviada: %.2f\n", temperatura);
 }
 
-void loop(){
+void setup() {
+  Serial.begin(115200);
+  // Conecta à rede WiFi
+  WiFi.begin(ssid, password);
+
+  pinMode(LED_PIN, OUTPUT);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  udp.begin(localPort);
+  Serial.printf("Servidor UDP: %s:%i \n", WiFi.localIP().toString().c_str(), localPort);
+  digitalWrite(LED_PIN, HIGH);
+
+  // Configura o ticker para enviar a temperatura a cada 2.5 segundos
+  sendTemperatureTicker.attach(2.5, SendTemperature);
+}
+
+void loop() {
   finalTempo = millis();
 
-  if ((finalTempo - inicioTempo) > 1000){
-    // setar o pwm para 0;
+  if ((finalTempo - inicioTempo) > 1000) {
+    // Setar o PWM para 0 e printar a mensagem
+    pwmRecebido = 0;
     Serial.println("Tempo excedido.");
+    inicioTempo = millis();
   }
 
   int packetSize = udp.parsePacket();
-  // Serial.print(" Received packet from : "); Serial.println(udp.remoteIP());
-  // Serial.print(" Size : "); Serial.println(packetSize);
   if (packetSize) {
     int pwm;
     udp.read((char*)&pwm, sizeof(pwm));
     ValidatePWM(pwm);
-    Serial.printf("pwm %d \n", pwmRecebido);  
+    Serial.printf("PWM recebido: %d\n", pwmRecebido);
   }
-  else{
-    inicioTempo = millis();
-  }
-
-
-
-  // Serial.println("\n");
-  // delay(500);
-  // Serial.print("[Server Connected] ");
-  // Serial.println (WiFi.localIP());
-
-
-
-
 }
+
