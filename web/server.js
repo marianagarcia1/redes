23a0f93712ee0ec8566316f1668b1894327cfbd1
Mauricio Godoy <mauriciomgodoy01@gmail.com>
2024-07-31 20:00:34 -0300

    atualizado

--- web/server.js
+++ web/server.js
@@ -1,8 +1,9 @@
 const dgram = require('dgram');
 const WebSocket = require('ws');
 
+// Configurações
 const udpPort = 12345; // Porta para receber dados UDP
-const wsPort = 8080;   // Porta do WebSocket
+const wsPort = 8080;   // Porta do servidor WebSocket
 
 // Criar um servidor UDP
 const udpServer = dgram.createSocket('udp4');
@@ -13,14 +14,26 @@ udpServer.on('listening', () => {
 
 udpServer.on('message', (message, rinfo) => {
     console.log(`Mensagem recebida: ${message} de ${rinfo.address}:${rinfo.port}`);
-    // Enviar a mensagem para todos os clientes WebSocket conectados
+
+    // Converter a mensagem para string e separar os valores
+    const data = message.toString().split(',');
+
+    // Convertendo os valores
+    const set_point = parseInt(data[0], 10);
+    const temperature = parseFloat(data[1]);
+
+    console.log(`Setpoint: ${set_point}°C`);
+    console.log(`Temperatura: ${temperature}°C`);
+
+    // Enviar para os clientes WebSocket conectados
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
-            client.send(message.toString());
+            client.send(JSON.stringify({ setpoint: set_point, temperature }));
         }
     });
 });
 
+// Iniciar o servidor UDP
 udpServer.bind(udpPort);
 
 // Criar um servidor WebSocket
@@ -31,4 +44,3 @@ wss.on('connection', ws => {
 });
 
 console.log(`Servidor WebSocket rodando na porta ${wsPort}`);
-
